514305584e61e203fe49af784b7471f73f5f8a9e
SATO Yusuke <yusuke.sato.zz@gmail.com>
2021-09-20 01:20:22 +0900

    Add implementation note

--- src/main/java/com/example/RateLimitter.java
+++ src/main/java/com/example/RateLimitter.java
@@ -10,11 +10,27 @@ import java.util.Set;
 /**
  * nftablesのlimit rateのような単位時間内でのアクセス数制限を行う。
  * Limit the number of access per a specified window like nftables "limit rate".
+ * 注意：
+ * allowAccess()によってアクセス可能と判定されたアクセスのみをアクセス数のカウント対象としている。
+ * そのため、アクセス不可の時間内に何度アクセス試行をしても、アクセス不可の時間が伸びることはない。
+ * アクセス不可の時間内もカウント対象に入れる場合、allowAccess()からaddAccess()を呼び出すタイミングを
+ * 判定処理の前に持ってくること。
  */
 public class RateLimitter {
     /** アクセスを追加順に保持するキュー */
+    /* 実装メモ：
+     * Java SE APIには、size()がO(1)で、peek()ができて、かつDequeでない単純なQueueの実装はない模様。
+     * 代案として、size()がO(1)で、peek()ができるArrayDequeのインスタンスを
+     * Queueインタフェース越しに操作している。
+     */
     Queue<Access> accessList = new ArrayDeque<>();
     /** アクセス数をアドレスごとに計数するためのマップ */
+    /* 実装メモ：
+     * Setには最低限アクセス時刻さえ格納されていれば処理は可能だが、
+     * CGNなどで同一アクセス元からのアクセスが同一ミリ秒内に発生した場合のハンドリングを避けるため
+     * Accessオブジェクトをそのまま格納している。
+     * Queueにも持っているオブジェクトのため、これにより消費メモリが増えることはないはず。
+     */
     Map<String, Set<Access>> accessMap = new HashMap<>();
     /** アクセス数を制限するタイムウィンドウ（ミリ秒） */
     private final long timeWindow;
@@ -44,6 +60,11 @@ public class RateLimitter {
      * @return アクセス可能な場合はtrue、アクセス不可の場合はfalse
      */
     public synchronized boolean allowAccess(Access newAccess) {
+        /* 実装メモ：
+         * 複数スレッドから同時に呼び出される可能性があるため、安全のためsynchronized指定している。
+         * リクエスト処理完了までロックする実装ではなく、処理時間も短いため問題にはならないと考えているが、
+         * 必要であればQueue・Map・SetをConcurrent処理に対応した実装に置き換えること。
+         */
         clearOldAccess(newAccess);
 
         Set<Access> targetAccesses = accessMap.get(newAccess.getAddress());
@@ -59,6 +80,10 @@ public class RateLimitter {
      * 渡されたアクセスをキューとマップに追加する。
      */
     private void addAccess(Access newAccess) {
+        /* 実装メモ：
+         * メソッドにくくりだしているのは、処理の見通しを良くするための他に、
+         * アクセス不可の時間内もカウント対象に入れたい場合の修正を容易にするため。
+         */
         accessList.add(newAccess);
         if (accessMap.get(newAccess.getAddress()) == null) {
             Set<Access> newSet = new HashSet<>();
@@ -73,8 +98,22 @@ public class RateLimitter {
      * 渡されたアクセスの時刻を基準に、古すぎる（タイムウィンドウを超過した）アクセスをキューとマップから削除する。
      */
     private void clearOldAccess(Access newAccess) {
+        /* 実装メモ：
+         * 1つのオブジェクトをQueueとSetの両方から参照していて
+         * 「Queueからオブジェクトを削除したら、Setからも同オブジェクトを削除する」という処理をしたい場合、
+         * 弱参照は使えない（Queueからオブジェクトが削除されても、Setからの参照はGCまでは生き残る）。
+         * そのためここではQueueとSetの両方から明示的にAccessオブジェクトを削除している。
+         */
         Access oldestAcceess = accessList.peek();
         while (oldestAcceess != null && oldestAcceess.getAccessTime() < newAccess.getAccessTime() - timeWindow) {
+            /* 実装メモ：
+             * メモリを不必要に食わないように、タイムウィンドウを超過したアクセスは
+             * 別アドレスからのアクセス含め削除していく。
+             * そのため、Queueにはアクセス元アドレス関係なしに追加された順にAccessオブジェクトを登録しておき
+             * Queueからオブジェクトをremoveしたらアクセス元アドレス関係なしに
+             * 古い順にアクセス記録が出てくるようにしておく。
+             * Setが空になったらSet自体もMapから削除する。
+             */
             accessList.poll();
             accessMap.get(oldestAcceess.getAddress()).remove(oldestAcceess);
             if (accessMap.get(oldestAcceess.getAddress()).isEmpty()) {
